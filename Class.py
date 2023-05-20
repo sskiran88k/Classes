ea93e730b5064445b508720948608b4f0da12877
S S Kiran <63164850+sskiran88k@users.noreply.github.com>
2023-05-20 16:04:27 +0000

    first commit

--- Class.py
+++ Class.py
@@ -1,10 +1,13 @@
 class kiran:
     var=10
     data=40
+    name="Kiran"
     def display(k):
         print("New Method")
     
 
-a=kiran()
+a=kiran() #Object Declaration
 print(a.var)
+print(a.data)
+print(a.name)
 a.display()
